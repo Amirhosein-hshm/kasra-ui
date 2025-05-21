20f6a159c4a065c0179a7a0d91e4f4a4d1771dac
Javad Mohammadi <javadsemail@gmail.com>
2025-05-21 09:49:27 +0330

    feat: dashboard page

---
+++ ui/pages/dashboard.page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-4xl font-bold">داشبورد</div>
+    </div>
+  );
+}
