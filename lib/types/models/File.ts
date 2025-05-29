87be9cc5a3f55b23d5722b463ae887d3baca7ac4
Javad Mohammadi <javadsemail@gmail.com>
2025-05-29 21:39:45 +0330

    feat: add required models

---
+++ lib/types/models/File.ts
@@ -0,0 +1,5 @@
+export default interface File {
+  id: number;
+  access: number;
+  file_loc: string;
+}
---
+++ lib/types/models/RFP.ts
@@ -0,0 +1,6 @@
+export default interface RFP {
+  id: number;
+  RFP_field_id: number;
+  file_id: number;
+  info: string;
+}
