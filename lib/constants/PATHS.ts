4f548e1b26cc489c73db031713d86ba3593f4ade
Javad Mohammadi <javadsemail@gmail.com>
2025-06-13 16:25:05 +0330

    feat: add all remaining routes

--- lib/constants/PATHS.ts
+++ lib/constants/PATHS.ts
@@ -7,19 +7,38 @@ export const PATHS = {
       root: '/dashboard',
       projects: {
         root: '/dashboard/projects',
+        single(id: number) {
+          return `/dashboard/projects/${id}`;
+        },
       },
       proposals: {
         root: '/dashboard/proposals',
+        single(id: number) {
+          return `/dashboard/proposals/${id}`;
+        },
+        upload: '/dashboard/proposals',
       },
       reports: {
         root: '/dashboard/reports',
+        single(id: number) {
+          return `/dashboard/reports/${id}`;
+        },
       },
       rfps: {
         root: '/dashboard/rfps',
+        create(id: number) {
+          return `/dashboard/rfps/create/${id}`;
+        },
+        update(id: number) {
+          return `/dashboard/rfps/update/${id}`;
+        },
       },
     },
     profile: {
       root: '/profile',
+    },
+    settings: {
+      root: '/settings',
     },
   },
 };
