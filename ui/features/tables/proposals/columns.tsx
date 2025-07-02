a31d6a492a30d9d563b969159aef5e287baf0550
amirhosein hashemi <amirhosein.hashemi.dev@gmail.com>
2025-07-02 07:11:05 -0600

    fix custom table

--- ui/features/tables/proposals/columns.tsx
+++ ui/features/tables/proposals/columns.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import { ProposalResponse } from 'lib/types/proposalResponse';
-import { Checkbox } from '@/ui/components/checkbox';
 import { ColumnDef } from '@tanstack/react-table';
-
-import { ArrowUpDown, Edit, Eye, MoreHorizontal, Trash } from 'lucide-react';
-
-import { Button } from '@/ui/components/button';
+import { Checkbox } from '@/ui/components/checkbox';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,85 +11,96 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/ui/components/dropdown-menu';
+import { Button } from '@/ui/components/button';
+import { ArrowUpDown, Edit, Eye, MoreHorizontal, Trash } from 'lucide-react';
 
-export const proposalsTableColumns: ColumnDef<ProposalResponse>[] = [
-  {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && 'indeterminate')
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="انتخاب همه"
-        className="m-3"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="انتخاب"
-        className="m-3"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => {
-      const proposal = row.original;
+const dropdownMenuItemClassname = 'justify-end cursor-pointer';
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">باز کردن فهرست</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel className="text-center">
-              فعالیت ها
-            </DropdownMenuLabel>
-
-            <DropdownMenuSeparator />
-
-            <DropdownMenuItem
-              onClick={() =>
-                navigator.clipboard.writeText(proposal.id.toString())
-              }
-              className={dropdownMenuItemClassname}
-            >
-              مشاهده <Eye color="var(--color-stone-primary)" />
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() =>
-                navigator.clipboard.writeText(proposal.id.toString())
-              }
-              className={dropdownMenuItemClassname}
-            >
-              حذف <Trash color="var(--color-red-primary)" />
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() =>
-                navigator.clipboard.writeText(proposal.id.toString())
-              }
-              className={dropdownMenuItemClassname}
-            >
-              ویرایش <Edit color="var(--color-blue-primary)" />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+export function getProposalsTableColumns(
+  userRoleId: number
+): ColumnDef<ProposalResponse>[] {
+  return [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && 'indeterminate')
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="انتخاب همه"
+          className="m-3"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="انتخاب"
+          className="m-3"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
     },
-  },
-  {
-    accessorKey: 'id',
-    header: ({ column }) => {
-      return (
+    {
+      id: 'actions',
+      cell: ({ row }) => {
+        const proposal = row.original;
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">باز کردن فهرست</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel className="text-center">
+                فعالیت ها
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem
+                onClick={() =>
+                  navigator.clipboard.writeText(proposal.id.toString())
+                }
+                className={dropdownMenuItemClassname}
+              >
+                مشاهده <Eye color="var(--color-stone-primary)" />
+              </DropdownMenuItem>
+
+              {userRoleId === 4 && (
+                <DropdownMenuItem
+                  onClick={() =>
+                    navigator.clipboard.writeText(proposal.id.toString())
+                  }
+                  className={dropdownMenuItemClassname}
+                >
+                  حذف <Trash color="var(--color-red-primary)" />
+                </DropdownMenuItem>
+              )}
+
+              {userRoleId === 4 && (
+                <DropdownMenuItem
+                  onClick={() =>
+                    navigator.clipboard.writeText(proposal.id.toString())
+                  }
+                  className={dropdownMenuItemClassname}
+                >
+                  ویرایش <Edit color="var(--color-blue-primary)" />
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+    {
+      accessorKey: 'id',
+      header: ({ column }) => (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
@@ -101,29 +108,27 @@ export const proposalsTableColumns: ColumnDef<ProposalResponse>[] = [
           شناسه
           <ArrowUpDown className="mr-2 h-4 w-4" />
         </Button>
-      );
+      ),
     },
-  },
-  {
-    accessorKey: 'info',
-    header: 'عنوان',
-  },
-  {
-    accessorKey: 'rfpId',
-    header: 'شناسه RFP',
-  },
-  {
-    accessorKey: 'userId',
-    header: 'شناسه کاربر',
-  },
-  {
-    accessorKey: 'comment',
-    header: 'توضیحات',
-  },
-  {
-    accessorKey: 'state',
-    header: 'وضعیت',
-  },
-];
-
-const dropdownMenuItemClassname = 'justify-end cursor-pointer';
+    {
+      accessorKey: 'info',
+      header: 'عنوان',
+    },
+    {
+      accessorKey: 'rfpId',
+      header: 'شناسه RFP',
+    },
+    {
+      accessorKey: 'userId',
+      header: 'شناسه کاربر',
+    },
+    {
+      accessorKey: 'comment',
+      header: 'توضیحات',
+    },
+    {
+      accessorKey: 'state',
+      header: 'وضعیت',
+    },
+  ];
+}
--- ui/features/tables/proposals/index.tsx
+++ ui/features/tables/proposals/index.tsx
@@ -1,11 +1,15 @@
 import { ProposalResponse } from 'lib/types/proposalResponse';
 import DataTable from '@/ui/components/data-table/index';
-import { proposalsTableColumns } from './columns';
+import { getProposalsTableColumns } from './columns';
+import { useMeStore } from '@/lib/stores/me.stores';
 
 interface Props {
   data: ProposalResponse[];
 }
 
 export default function ProposalsTable({ data }: Props) {
+  const userTypeId = useMeStore((s) => s.user?.userTypeId);
+  const proposalsTableColumns = getProposalsTableColumns(userTypeId!);
+
   return <DataTable columns={proposalsTableColumns} data={data} />;
 }
