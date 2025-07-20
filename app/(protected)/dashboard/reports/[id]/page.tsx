78f67c65ca4706cbc6627b4ca9db015bb7dfd066
Javad Mohammadi <javadsemail@gmail.com>
2025-07-20 10:27:04 +0330

    feat: single report page

--- app/(protected)/dashboard/reports/[id]/page.tsx
+++ app/(protected)/dashboard/reports/[id]/page.tsx
@@ -1,3 +1,45 @@
+import { getSupervisor } from '@/lib/services';
+import { ProjectResponse } from '@/lib/types';
+import ErrorView from '@/ui/features/error/error.view';
 import SingleReportPage from '@/ui/pages/reports/single-report.page';
 
-export default SingleReportPage;
+import { QueryClient } from '@tanstack/react-query';
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  const reportId = Number(id);
+
+  const queryClient = new QueryClient();
+
+  // Prefetch the data on the server
+  //   await queryClient.prefetchQuery({
+  //     queryKey: ['supervisorSingleProject', reportId],
+  //     queryFn: () =>
+  //       getSupervisor()
+  //         .readProjectsSupervisorSingleProjectProjectIdGet(reportId)
+  //         .then((res) => {
+  //           console.log(res.data);
+  //           return res.data;
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         }),
+  //   });
+
+  //   const reportData: ProjectResponse | undefined = queryClient.getQueryData([
+  //     'supervisorSingleReport',
+  //     reportId,
+  //   ]);
+
+  //   if (!reportData) {
+  //     return <ErrorView />;
+  //   }
+
+  return <SingleReportPage />;
+}
--- ui/pages/reports/single-report.page.tsx
+++ ui/pages/reports/single-report.page.tsx
@@ -1,3 +1,32 @@
+import { Badge } from '@/ui/components/badge';
+import FileDownload from '@/ui/components/file-download';
+import Link from 'next/link';
+
 export default function SingleReportPage() {
-  return <h1>Single Report Page</h1>;
+  return (
+    <div className="mt-4 flex flex-col gap-2">
+      <Badge>پروژه ۱</Badge>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        facilis ipsa doloremque molestias eaque cupiditate dolor eius fuga ab a
+        facere, aut, architecto voluptate, nisi temporibus quisquam asperiores
+        excepturi dicta pariatur in hic possimus ipsam? Corporis, vel!
+        Temporibus pariatur cupiditate dolore dolorum necessitatibus nihil!
+        Ducimus eius aut unde omnis maxime doloribus, pariatur cum numquam error
+        fugit laudantium, veritatis eos facere nisi consectetur dolor placeat
+        vero repellat consequuntur tempora sequi. Quaerat velit aspernatur harum
+        consequuntur impedit porro explicabo amet. Fuga vero beatae repellendus
+        rem temporibus magnam minima accusamus, quae perspiciatis provident
+        doloremque at corrupti iste consequuntur a. Voluptate veniam illum
+        adipisci!
+      </p>
+
+      <div className="flex gap-2">
+        <Link href={''}>
+          <FileDownload title="بارگیری پروپوزال" />
+        </Link>
+      </div>
+    </div>
+  );
 }
