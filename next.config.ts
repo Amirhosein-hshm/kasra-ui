00f9b04ae6d581ec3aaf97ac05bd5b90542126d1
amirhosein hashemi <amirhosein.hashemi.dev@gmail.com>
2025-07-30 02:22:19 -0600

    🐛 fix: remove unnecessary environment variable from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: 'http://localhost:8000/',
-  },
+  env: {},
 };
 
 export default nextConfig;
