039bdede6167b610ea1ed36d9799c37c84166683
Javad Mohammadi <javadsemail@gmail.com>
2025-08-05 13:39:55 +0330

    🔥 Remove redundant env vars from next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 // next.config.ts
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: 'http://localhost:8000/',
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
