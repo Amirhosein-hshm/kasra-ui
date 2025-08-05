e7be1013b2b817078e1dc9ebd5cf89df630b9feb
Javad Mohammadi <javadsemail@gmail.com>
2025-08-05 18:59:36 +0330

    🐛 Fix file-download internal structure

--- ui/components/file-download.tsx
+++ ui/components/file-download.tsx
@@ -1,7 +1,15 @@
 'use client';
+import { IconDownload } from '@tabler/icons-react';
 import { Button, ButtonProps } from './button';
 
 export const FileDownload = (props: ButtonProps) => {
-  const { children, ...rest } = props;
-  return <Button {...rest}>{children ?? 'بارگیری فایل'}</Button>;
+  const { children, variant, ...rest } = props;
+  return (
+    <Button variant={variant ?? "outline"} {...rest}>
+      <div className="flex gap-2">
+        <IconDownload />
+        <span>{children ?? 'بارگیری فایل'}</span>
+      </div>
+    </Button>
+  );
 };
