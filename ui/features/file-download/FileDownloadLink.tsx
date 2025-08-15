bc912d4667080949efda48c6c10be5a801de1136
Javad Mohammadi <javadsemail@gmail.com>
2025-08-15 18:40:05 +0330

    ✨ Add loading view for FileDownloadLink

--- ui/features/file-download/FileDownloadLink.tsx
+++ ui/features/file-download/FileDownloadLink.tsx
@@ -1,22 +1,28 @@
 'use client';
-import { useDownload } from '@/lib/hooks/useDownload';
 import { useCreateDownload } from '@/lib/utils/hooks/useCreateDownload';
-import { toast } from 'sonner';
+import Spinner from '@/ui/common/spinner';
+import clsx from 'clsx';
 
 type Props = {
   id: number;
 };
 
 export default function FileDownloadLink({ id }: Props) {
-  const { onDownLoad } = useCreateDownload(id);
+  const { onDownLoad, isPending } = useCreateDownload(id);
   return (
     id && (
       <div
-        className="inline-block text-blue-600 dark:text-blue-400 hover:underline mt-2"
-        onClick={onDownLoad}
-        style={{ cursor: 'pointer' }}
+        className={clsx(
+          'inline-flex gap-2 text-blue-600 dark:text-blue-400 hover:underline mt-2',
+          isPending && 'opacity-50'
+        )}
+        onClick={() => {
+          if (!isPending) onDownLoad();
+        }}
+        style={isPending ? { cursor: 'pointer' } : {}}
       >
         📎 دانلود فایل پیوست‌شده
+        {isPending && <Spinner />}
       </div>
     )
   );
